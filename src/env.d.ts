1632def2c3402a9ea2aa5e82f035af490c793914
Sean Swanson <seancswan@gmail.com>
2022-12-20 16:45:15 -0800

    Added Umami Analytics

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="@astrojs/image/client" />
+interface ImportMetaEnv {
+    readonly PUBLIC_UMAMI: string;
+    // more env variables...
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
