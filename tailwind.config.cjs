3e60c7d4fd0f3c4a240a6e70ff0a34bd9a3c0f3e
Sean Swanson <seancswan@gmail.com>
2022-12-24 18:07:35 -0800

    Styling refactors.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,19 +7,10 @@ module.exports = {
 				skinShadow: 'rgb(var(--color-shadow) / <alpha-value>)'
 			},
 			fontFamily: {
-				'spaceMono': "'Space Mono', ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, Liberation Mono, Courier New, monospace"
-			},
-			backgroundImage: {
-				portrait: "url('/src/images/portrait.jpg')",
-				davines: "url('/src/images/davines.jpg')",
-				aboutMe1: "url('/src/images/about-me-1.jpg')",
-				aboutMe2: "url('/src/images/about-me-2.jpg')"
+				skinDisplay: 'var(--font-family-skin-display)',
+				skinMono: "'Space Mono', ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, Liberation Mono, Courier New, monospace"
 			},
 			backgroundColor: {
-				brandGreen: "rgb(187 247 208)",
-				brandBlue: "#FDEBF7",
-				brandFloralWhite: "floralwhite",
-				brandWheat: "#a1843a26",
 				skin: {
 					base: 'var(--color-bg-base)',
 					accent: 'var(--color-bg-accent)',
@@ -29,15 +20,11 @@ module.exports = {
 				'50': '50px',
 				'160': '160px'
 			},
-			fontFamily: {
-				abuget: 'Abuget'
-			},
 			minWidth: {
 				'475': '475px'
 			},
-			backgroundPosition: {
-				shiftUp: "0px -40px",
-				shiftUpSm: "0px -10px"
+			maxWidth: {
+				'475': '475px'
 			},
 			textColor:
 			{
